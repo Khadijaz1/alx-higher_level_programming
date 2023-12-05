ad1527aa58dcb0ad926026d88c7235c38c0f7333
Khadijaz1 <khadujizamil@gmail.com>
2023-12-05 12:17:05 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,3 @@ def no_c(my_string):
     new_sentence = my_string.translate({ord('c') : None})
     new_sentence = my_string.translate({ord('C') : None})
     return new_sentence
-
-print(no_c("The letter comes nothing"))
