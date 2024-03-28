8f05119f32d3c4ad2d64de9ede1d2025acc4eb73
Khadijaz1 <khadujizamil@gmail.com>
2024-03-28 14:04:23 -0700

    all

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-# script that takes in a URL and displays all HTTP methods the server will accept
+
+# Check if URL argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send OPTIONS request to the server and display allowed methods
+curl -s -X OPTIONS -i "$1" | grep -i allow | tr -d '\r' | awk '{print "Allowed HTTP Methods:", $3}'
